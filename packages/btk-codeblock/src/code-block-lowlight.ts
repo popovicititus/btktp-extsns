dec8e59ec9d8ed50d723fa45f93f9b4dde84ff77
Titus <popovici.titus@yahoo.com>
2022-10-04 06:19:59 +0300

    initial commit

---
+++ packages/btk-codeblock/src/code-block-lowlight.ts
@@ -0,0 +1,62 @@
+import {lowlight} from 'lowlight/lib/core';
+import CodeBlock, {CodeBlockOptions} from '@tiptap/extension-code-block';
+import {LowlightPlugin} from './lowlight-plugin';
+
+export interface CodeBlockLowlightOptions extends CodeBlockOptions {
+  lowlight: any;
+  defaultLanguage: string | null | undefined;
+}
+
+export const BtmkCodeBlock = CodeBlock.extend<CodeBlockLowlightOptions>({
+  addOptions() {
+    return {
+      ...this.parent?.(),
+      lowlight,
+      defaultLanguage: null,
+    };
+  },
+
+  addAttributes() {
+    return {
+      language: {
+        default: 'plaintext'
+      },
+      codeLanguage: {
+        default: 'plaintext'
+      }
+    };
+  },
+
+  addProseMirrorPlugins() {
+    return [
+      ...this.parent?.() || [],
+      LowlightPlugin({
+        name: this.name,
+        lowlight: this.options.lowlight,
+        defaultLanguage: this.options.defaultLanguage,
+      }),
+    ];
+  },
+
+  addCommands() {
+    return {
+      ...this.parent?.(),
+      setCodeblockLanguage: (language) => ({commands}) => {
+        return commands.updateAttributes('codeBlock', {
+          codeLanguage: language,
+          language: language,
+          class: 'language-' + language
+        });
+      },
+      toggleCodeblockColoring: () => ({commands}) => {
+        const attrs = this.editor.getAttributes('codeBlock');
+        if (attrs?.language !== 'plaintext') {
+          return commands.updateAttributes('codeBlock', {language: 'plaintext', class: 'language-plaintext'});
+        } else {
+          const lang = attrs?.codeLanguage || 'plaintext';
+          return commands.updateAttributes('codeBlock', {language: lang, class: 'language-' + lang});
+        }
+      }
+    };
+  },
+});
---
+++ packages/btk-codeblock/src/index.ts
@@ -0,0 +1,5 @@
+import { BtmkCodeBlock } from './code-block-lowlight';
+
+export * from './code-block-lowlight';
+
+export default BtmkCodeBlock;
